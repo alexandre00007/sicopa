2e1cdaacd48e5fad86610eaa1bb2e01d64a46033
alexandre00007 <58002843+alexandre00007@users.noreply.github.com>
2026-08-14 16:39:03 +0100

    feat: activer la suppression des rapprochements

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import logging
 import tkinter as tk
 from tkinter import messagebox
 
-from controle_paie.raw_fusion_period_app import PayrollAppWithPeriodAwareRawFusion as PayrollApp
+from controle_paie.matching_deletion_app import PayrollAppWithMatchingDeletion as PayrollApp
 
 
 if __name__ == "__main__":
