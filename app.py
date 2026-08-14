13e17518f39e92d67837165febdf117040622433
alexandre00007 <58002843+alexandre00007@users.noreply.github.com>
2026-08-14 20:26:22 +0100

    feat: activer les controles de coherence stricte

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import logging
 import tkinter as tk
 from tkinter import messagebox
 
-from controle_paie.task_managed_app import PayrollAppWithTaskManager as PayrollApp
+from controle_paie.strict_consistency_app import PayrollAppWithStrictConsistency as PayrollApp
 
 
 if __name__ == "__main__":
