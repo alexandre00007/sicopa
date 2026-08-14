1bc17e8804969ca922553b22153c55c0d1d07e6d
alexandre00007 <58002843+alexandre00007@users.noreply.github.com>
2026-08-14 12:53:43 +0100

    feat: activer la console SQL dans l'application

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import logging
 import tkinter as tk
 from tkinter import messagebox
 
-from controle_paie.regime_comparison_app import PayrollAppWithFinalRegimeComparison as PayrollApp
+from controle_paie.sql_console_app import PayrollAppWithSqlConsole as PayrollApp
 
 
 if __name__ == "__main__":
