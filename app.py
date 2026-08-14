9bc3cf69313d3b047ff34fb75a31791b3539a866
alexandre00007 <58002843+alexandre00007@users.noreply.github.com>
2026-08-14 18:18:49 +0100

    feat: activer comparaison RAW par periode dans application unifiee

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import logging
 import tkinter as tk
 from tkinter import messagebox
 
-from controle_paie.sql_console_unified_enhanced import PayrollAppUnified as PayrollApp
+from controle_paie.raw_period_comparison_app import PayrollAppWithRawPeriodComparison as PayrollApp
 
 
 if __name__ == "__main__":
