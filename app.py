44899760f4729d21b1bc58e33103d25431145ab9
alexandre00007 <58002843+alexandre00007@users.noreply.github.com>
2026-08-15 01:32:33 +0100

    feat: activer architecture donnees Lot 2

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import logging
 import tkinter as tk
 from tkinter import messagebox
 
-from controle_paie.export_reliability_app import PayrollAppWithReliableExports as PayrollApp
+from controle_paie.data_architecture_app import PayrollAppWithDataArchitecture as PayrollApp
 
 
 if __name__ == "__main__":
