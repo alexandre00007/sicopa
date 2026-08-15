ccdaa3e336f1d8e11173f7fe8652155f8725bd4b
alexandre00007 <58002843+alexandre00007@users.noreply.github.com>
2026-08-15 01:44:46 +0100

    feat: activer lot 3 sante et performance

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import logging
 import tkinter as tk
 from tkinter import messagebox
 
-from controle_paie.data_architecture_final_app import PayrollAppWithFinalDataArchitecture as PayrollApp
+from controle_paie.performance_health_app import PayrollAppWithPerformanceHealth as PayrollApp
 
 
 if __name__ == "__main__":
